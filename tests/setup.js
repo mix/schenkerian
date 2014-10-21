dcb987488100ed8b526aad9fd23f7a4720c00613
Dustin Diaz <polvero@gmail.com>
2014-10-21 12:50:37 -0700

    setup for tests

---
+++ tests/setup.js
@@ -0,0 +1,17 @@
+var chai = require('chai')
+  , sinonChai = require('sinon-chai')
+  , promised = require('chai-as-promised')
+  , v = require('valentine')
+
+global.expect = chai.expect
+global.noop = function () {
+  return function () {}
+}
+global.sinon = require('sinon')
+chai.use(promised)
+chai.use(sinonChai)
+
+process.on('uncaughtException', function (err) {
+  console.error('Uncaught exception', err, err.stack)
+  process.exit(3)
+})
